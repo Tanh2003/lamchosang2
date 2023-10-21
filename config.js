58c39d7081e50e414e58146e52094b9b7425917f
Tanh2003 <nhoxtuananh092@gmail.com>
2023-10-21 22:06:37 +0700

    lan2

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Bé  của Anh';
+const giftUrl = 'https://www.facebook.com/mons.nho.77';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Tặng Bé  của anh';
+const contentCard = 'Chúc bé của anh 20/10 tràn ngập niềm vui và những nụ cười  ❤️. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau yêu em lắm á💋💋💋💋';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'anh1.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
